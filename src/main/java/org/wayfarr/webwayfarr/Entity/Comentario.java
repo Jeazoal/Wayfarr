a6eb46c4bc028577ceda63dcb97a173e42f3b3e2
Fazt <contact@faztweb.com>
2024-03-15 01:07:04 -0500

    no se mano

--- src/main/java/org/wayfarr/webwayfarr/Entity/Comentario.java
+++ src/main/java/org/wayfarr/webwayfarr/Entity/Comentario.java
@@ -1,12 +1,18 @@
 package org.wayfarr.webwayfarr.Entity;
-
-import jakarta.persistence.*;
-import lombok.Data;
-import org.springframework.transaction.annotation.Transactional;
 import org.wayfarr.webwayfarr.Shared.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.transaction.Transactional;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 
-@Entity
 @Data
+@EqualsAndHashCode(callSuper = false)
+@Entity
 @Transactional
 @Table(name = "Comentarios")
 public class Comentario extends BaseEntity {
