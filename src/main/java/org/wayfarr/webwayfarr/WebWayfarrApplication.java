adcf415a87199e577bbaa74382b55b5d8922662d
Jeazoal <alonsomarzano@gmail.com>
2024-03-14 19:57:04 -0500

    V1

---
+++ src/main/java/org/wayfarr/webwayfarr/WebWayfarrApplication.java
@@ -0,0 +1,13 @@
+package org.wayfarr.webwayfarr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebWayfarrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebWayfarrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/wayfarr/webwayfarr/WebWayfarrApplicationTests.java
@@ -0,0 +1,13 @@
+package org.wayfarr.webwayfarr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebWayfarrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
