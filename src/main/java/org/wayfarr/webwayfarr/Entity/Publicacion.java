6d2505cc999dc937d4c9c32b7ed100e69654ecd1
Feiky <jeanpierreplp@gmail.com>
2024-03-15 00:27:24 -0500

    toma mielda

--- src/main/java/org/wayfarr/webwayfarr/Entity/Publicacion.java
+++ src/main/java/org/wayfarr/webwayfarr/Entity/Publicacion.java
@@ -1,12 +1,23 @@
 package org.wayfarr.webwayfarr.Entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.Data;
 
 @Data
 public class Publicacion {
+    @Id
+    @Column(name = "id_publicacion")
     private Integer idpubli;
+    @Column(name = "imagen_url")
     private String imagenUrl;
+    @Column(name = "descripcion")
     private String descripcion;
+    @Column(name = "direccion ")
     private String direccion;
+    @ManyToOne
+    @JoinColumn(name = "id_usuario", referencedColumnName = "id_user")
     private User user;
 }
